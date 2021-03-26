9efbf048029950d58c844aafea435bd7cf68e7ce
Ben Laplanche <ben@laplanche.co.uk>
2021-03-26 20:00:00 +0000

    switched to terraform cloud example

--- s3.tf
+++ s3.tf
@@ -1,16 +1,38 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
+  backend "remote" {
+    organization = "benlaplanche"
+
+    workspaces {
+      name = "gh-actions-demo"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = ".aws/credentials"
+  region = "us-east-1"
+  # shared_credentials_file = ".aws/credentials"
 }
 
 variable "s3_acl" {
